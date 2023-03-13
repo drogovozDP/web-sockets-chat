50336b30e2f37908b96ad1465b8d225eef4b9792
Dima Drogovoz <drogowoz@gmail.com>
2023-03-13 21:19:17 +0400

    test

--- frontend/src/content/Chat.jsx
+++ frontend/src/content/Chat.jsx
@@ -4,6 +4,7 @@ import {fetchToken, setToken} from "./Auth";
 import "./css/Chat.css";
 // import { ws } from "./App"
 
+
 export default class Chat extends React.Component {
     constructor(props) {
         super(props);
